af767f719bda68c525b7e76064119407db109347
Abdul Dakkak <abduld@wolfram.com>
2018-06-20 18:22:25 -0500

    updates

---
+++ hash/pbkdf2/raw/maxrounds_32bit.go
@@ -0,0 +1,7 @@
+// +build 386 armv5 armv7 armv6
+
+package raw
+
+const (
+	MaxRounds = 0xffff
+)
--- hash/pbkdf2/raw/pbkdf2.go
+++ hash/pbkdf2/raw/pbkdf2.go
@@ -2,8 +2,6 @@ package raw
 
 import (
 	"hash"
-	"math"
-	"strconv"
 
 	"golang.org/x/crypto/pbkdf2"
 )
@@ -12,18 +10,6 @@ const (
 	MinRounds = 1
 )
 
-var (
-	MaxRounds int
-)
-
 func Hash(password, salt []byte, rounds int, hf func() hash.Hash) (hash string) {
 	return Base64Encode(pbkdf2.Key(password, salt, rounds, hf().Size(), hf))
-}
-
-func init() {
-	if strconv.IntSize == 4 {
-		MaxRounds = math.MaxInt32
-	} else {
-		MaxRounds = math.MaxInt64
-	}
 }
